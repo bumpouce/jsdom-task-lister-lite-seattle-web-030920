2879b696650593ba125a182516cf969f5ab0b126
christine <bumpouce@gmail.com>
2020-04-21 21:18:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.getElementById("new-task-description")
   const taskForm = document.getElementById("create-task-form")
   const taskList = document.getElementById("tasks")
+  const sortListAction = document.getElementById("sort")
 
   taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -25,20 +26,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
     inputField.value = ""
 
-    sortListItems(taskList);
+    sortListAction.addEventListener("submit", (e) => {
+      e.preventDefault();
+      debugger
+      sortList();
+    })
+  
   })
 
-  function sortListItems(parentEl){
-    let newArray = Array.from(parentEl.children)
-    // debugger
 
-    let high, medium, low
-    high = 1;
-    medium = 2; 
-    low = 3;
+  function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = taskList;
+    switching = true;
 
-    function compare(a, b) { if (a > b) return 1; if (b > a) return -1; return 0; } 
-    // .forEach(function (element) { element }); 
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+    
+      // Start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("li");
+    
+      // Loop through all list items:
+      for (i = 0; i < (b.length - 1); i++) {
+    
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+    
+        /* Check if the next item should
+        switch place with the current item: */
+        if (b[i].value > b[i + 1].value) {
+    
+          /* If next item is alphabetically lower than current item,
+          mark as a switch and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+    
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
   }
 
 
